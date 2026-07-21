9dc076dc3c4bc85bb2bc54467836e8777e1df181
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-22 02:26:23 +0530

    java: refactoring the code and making it more scalable; making the user be able to give out inputs of their own

--- frontBack.java
+++ frontBack.java
@@ -1,18 +1,32 @@
+import java.util.*;
+
 public class frontBack{
     public static void main(String[] args){
-        String printer = frontback("code");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String printer = frontback(input);
         System.out.println(printer);
+
+        sc.close();
     }
 
     public static String frontback(String str){
         String newstr = "";
         
-        int number = str.length();
-        char last = str.charAt(number-1);
-        for (int i = 1; i < str.length()-1; i++){
-            newstr += str.charAt(i);
+        // wait i think i can make this better
+
+        if (str.length() > 1){
+            int number = str.length();
+            char last = str.charAt(number-1);
+            for (int i = 1; i < str.length()-1; i++){
+                newstr += str.charAt(i);
+            }
+            char first = str.charAt(0);
+            return (last + newstr + first);
+        }else{
+            newstr = str;
+            return str;
         }
-        char first = str.charAt(0);
-        return (last + newstr + first);
+        
     }
 }

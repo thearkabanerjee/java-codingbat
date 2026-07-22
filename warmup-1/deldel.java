db04d8ac5ae43c8f46f942b512420a36d1955d3e
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-23 01:21:02 +0530

    java: del del

---
+++ warmup-1/deldel.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class deldel{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        
+
+        sc.close();
+    }
+
+    public static String del(String str){
+        // there goes the rest of it
+    }
+}
